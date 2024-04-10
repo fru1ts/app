830b4b1e0bf3c90cd051d498ebdf893310d295b0
fru1ts <2677767518@qq.com>
2024-04-10 10:19:59 +0800

    粤农辞画

--- yuefangyan/www/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$dbhost="db";
-$port="3306";
-$dbuser="root";
-$dbpwd="Qaz123!@#";
-$dbname="app";
-?>
--- yuefangyan/www/include/common.php
+++
@@ -1,5 +0,0 @@
-<?php
-error_reporting(0);
-define('ROOT', dirname(__FILE__).'/');
-include ROOT.'../config.php';
-?>
